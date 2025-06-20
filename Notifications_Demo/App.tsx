d70e035b492ec148cee7cec3813f0c9c60fdcbf6
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-06-20 18:28:53 +0500

    called usePushNotifications hook and displaying push token

--- Notifications_Demo/App.tsx
+++ Notifications_Demo/App.tsx
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { usePushNotifications } from "./src/services/notifications";
 
 export default function App() {
+  const { expoPushToken, notification } = usePushNotifications();
+  const data = JSON.stringify(notification, undefined, 2);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Expo Push Token: {expoPushToken?.data}</Text>
+      <Text>Notification: {data}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
